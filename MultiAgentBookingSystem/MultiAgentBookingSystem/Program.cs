03d84be6201b92fff078a9ee5b38100388c0c9ca
Tomasz Glowka <tomek3396@gmail.com>
2020-05-12 22:46:41 +0200

    Add .gitignore and basic project structure

---
+++ MultiAgentBookingSystem/MultiAgentBookingSystem/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Akka.Actor;
+
+namespace MultiAgentBookingSystem
+{
+    class Program
+    {
+
+        private static ActorSystem TicketBookingActorSystem;
+        static void Main(string[] args)
+        {
+            TicketBookingActorSystem = ActorSystem.Create("TicketBookingActorSystem");
+
+            Console.WriteLine("Hello World");
+            Console.ReadKey();
+
+            TicketBookingActorSystem.Terminate();
+        }
+    }
+}
